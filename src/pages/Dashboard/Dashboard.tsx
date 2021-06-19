66a315a2553189d594e6475e07fe6465b6496ec5
Alexnorvag <norvag@mail.ru>
2021-06-19 03:53:16 +0300

    added: table on cell click menu; table styles;

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,64 @@
-import React, { FC } from "react";
+import React, { FC, useState, MouseEvent } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import {
+  createStyles,
+  Menu,
+  MenuItem,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Theme,
+} from "@material-ui/core";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    tableContainer: {
+      background: "transparent",
+    },
+    table: {
+      minWidth: 650,
+    },
+    tableHead: {
+      border: "2px solid #929497",
+    },
+    tableHeadCell: {
+      color: "#929497",
+      fontSize: "20px",
+      textTransform: "uppercase",
+      lineHeight: "35px",
+    },
+    tableCell: {
+      color: "#fff",
+      fontSize: "20px",
+      lineHeight: "35px",
+    },
+    menuContainer: {
+      background: "#929497",
+      borderRadius: "unset",
+      border: "solid 2px #fff",
+      padding: theme.spacing(2),
+      minWidth: "160px",
+    },
+    menuList: {
+      padding: 0,
+    },
+    menuItem: {
+      fontSize: "20px",
+      textTransform: "uppercase",
+      color: "#fff",
+      lineHeight: "35px",
+    },
+    tableRow: {
+      "&:hover": {
+        background: "#004EFD4B !important",
+      },
+    },
+  })
+);
 
 function createData(
   projectName: string,
@@ -37,35 +83,157 @@ const rows = [
 export const Dashboard: FC = () => {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: MouseEvent<HTMLTableHeaderCellElement>) => {
+    console.log("event.currentTarget: ", event.currentTarget);
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div style={{ color: "red" }}>
-      <TableContainer component={Paper}>
+      <TableContainer component={Paper} className={classes.tableContainer}>
         <Table
           className={classes.table}
           size="small"
           aria-label="a dense table"
         >
-          <TableHead>
+          <TableHead className={classes.tableHead}>
             <TableRow>
-              <TableCell align="right">PROJECT NAME</TableCell>
-              <TableCell align="right">STATUS</TableCell>
-              <TableCell align="right">NEXT REVIEW</TableCell>
-              <TableCell align="right">MILESTONES</TableCell>
-              <TableCell align="right">DELIVERY</TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">
+                PROJECT NAME
+              </TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">
+                STATUS
+              </TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">
+                NEXT REVIEW
+              </TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">
+                MILESTONES
+              </TableCell>
+              <TableCell className={classes.tableHeadCell} align="right">
+                DELIVERY
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <TableRow key={row.projectName}>
-                <TableCell component="th" scope="row">
+              <TableRow
+                key={row.projectName}
+                hover
+                className={classes.tableRow}
+              >
+                <TableCell
+                  className={classes.tableCell}
+                  component="th"
+                  scope="row"
+                  onClick={handleClick}
+                >
                   {row.projectName}
                 </TableCell>
-                <TableCell align="right">{row.status}</TableCell>
-                <TableCell align="right">{row.nextReview}</TableCell>
-                <TableCell align="right">{row.milestones}</TableCell>
-                <TableCell align="right">{row.delivery}</TableCell>
+                <TableCell
+                  className={classes.tableCell}
+                  align="right"
+                  onClick={handleClick}
+                >
+                  {row.status}
+                </TableCell>
+                <TableCell
+                  className={classes.tableCell}
+                  align="right"
+                  onClick={handleClick}
+                >
+                  {row.nextReview}
+                </TableCell>
+                <TableCell
+                  className={classes.tableCell}
+                  align="right"
+                  onClick={handleClick}
+                >
+                  {row.milestones}
+                </TableCell>
+                <TableCell
+                  className={classes.tableCell}
+                  align="right"
+                  onClick={handleClick}
+                >
+                  {row.delivery}
+                </TableCell>
               </TableRow>
             ))}
+
+            <Menu
+              classes={{
+                paper: classes.menuContainer, // class name, e.g. `classes-nesting-root-x`
+                list: classes.menuList,
+              }}
+              id="card-actions-menu"
+              anchorEl={anchorEl}
+              getContentAnchorEl={null}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              transformOrigin={{ vertical: "top", horizontal: "center" }}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem
+                classes={{
+                  root: classes.menuItem,
+                }}
+                disableGutters
+                onClick={() => {
+                  // TODO: change to real handlers
+                  console.log("Open action");
+                  handleClose();
+                }}
+              >
+                Open
+              </MenuItem>
+              <MenuItem
+                classes={{
+                  root: classes.menuItem,
+                }}
+                disableGutters
+                onClick={() => {
+                  // TODO: change to real handlers
+                  console.log("Edit action");
+                  handleClose();
+                }}
+              >
+                Edit
+              </MenuItem>
+              <MenuItem
+                classes={{
+                  root: classes.menuItem,
+                }}
+                disableGutters
+                onClick={() => {
+                  // TODO: change to real handlers
+                  console.log("Duplicate action");
+                  handleClose();
+                }}
+              >
+                Duplicate
+              </MenuItem>
+              <MenuItem
+                classes={{
+                  root: classes.menuItem,
+                }}
+                disableGutters
+                onClick={() => {
+                  // TODO: change to real handlers
+                  console.log("Download action");
+                  handleClose();
+                }}
+              >
+                Download
+              </MenuItem>
+            </Menu>
           </TableBody>
         </Table>
       </TableContainer>
