4f77573f871fdc8a88a08dc8a3816ecdf246c721
Fondandry <valentingavron19991672@mail.ru>
2021-06-21 22:04:33 +0300

    fix: sidebar color

--- src/assets/icons/ImageRoundedIcon.tsx
+++ src/assets/icons/ImageRoundedIcon.tsx
@@ -14,32 +14,32 @@ export const ImageRoundedIcon: FC<ImageRoundedIconProps> = ({ fill }) => {
       fill={fill}
     >
       <g
-        id="Сгруппировать_857"
-        data-name="Сгруппировать 857"
+        id="Group_857"
+        data-name="Group 857"
         transform="translate(-20 -329)"
       >
-        <g id="Сгруппировать_856" data-name="Сгруппировать 856">
+        <g id="Group_856" data-name="Group 856">
           <g
             id="Camera_free_icon_8"
             data-name="Camera free icon 8"
             transform="translate(20 329)"
           >
             <path
-              id="Контур_612"
-              data-name="Контур 612"
+              id="contour_612"
+              data-name="contour 612"
               d="M32.75,0A32.75,32.75,0,1,0,65.5,32.75,32.75,32.75,0,0,0,32.75,0Zm0,57.313A24.563,24.563,0,1,1,57.313,32.75,24.591,24.591,0,0,1,32.75,57.313Z"
             />
           </g>
         </g>
         <path
-          id="Контур_616"
-          data-name="Контур 616"
+          id="contour_616"
+          data-name="contour 616"
           d="M106.285,218.105a2.468,2.468,0,0,0-3.982,0l-10.95,14.113a2.478,2.478,0,0,1-4.12-.193l-4.688-7.208a2.485,2.485,0,0,0-4.257,0L71.1,235.871c-1.237,1.9,0,4.543,2.128,4.543h44.7c2.231,0,3.438-2.872,1.991-4.737Z"
           transform="translate(-43 139.3)"
         />
         <path
-          id="Контур_617"
-          data-name="Контур 617"
+          id="contour_617"
+          data-name="contour 617"
           d="M149.383,148.38a5.012,5.012,0,1,0-5.012-5.012A5.012,5.012,0,0,0,149.383,148.38Z"
           transform="translate(-106.83 207.434)"
         />
--- src/assets/icons/MenuIcon.tsx
+++ src/assets/icons/MenuIcon.tsx
@@ -11,35 +11,36 @@ export const MenuIcon: FC<MenuIconProps> = ({ fill }) => {
       width="65.5"
       height="65.5"
       viewBox="0 0 65.5 65.5"
+      fill={fill}
     >
       <g
-        id="Сгруппировать_855"
-        data-name="Сгруппировать 855"
+        id="Group_855"
+        data-name="Group 855"
         transform="translate(-20 -723)"
       >
         <g id="menu" transform="translate(22 725)">
           <path
-            id="Контур_52"
-            data-name="Контур 52"
+            id="contur_52"
+            data-name="contur 52"
             d="M179.156,154.168H151.75a2.418,2.418,0,1,1,0-4.836h27.406a2.418,2.418,0,0,1,0,4.836Zm0,0"
             transform="translate(-134.703 -131.703)"
           />
           <path
-            id="Контур_53"
-            data-name="Контур 53"
+            id="contur_53"
+            data-name="contur 53"
             d="M179.156,244.836H151.75a2.418,2.418,0,1,1,0-4.836h27.406a2.418,2.418,0,0,1,0,4.836Zm0,0"
             transform="translate(-134.703 -211.668)"
           />
           <path
-            id="Контур_54"
-            data-name="Контур 54"
+            id="contur_54"
+            data-name="contur 54"
             d="M179.156,335.5H151.75a2.418,2.418,0,1,1,0-4.836h27.406a2.418,2.418,0,0,1,0,4.836Zm0,0"
             transform="translate(-134.703 -291.633)"
           />
         </g>
         <path
-          id="Контур_615"
-          data-name="Контур 615"
+          id="contur_615"
+          data-name="contur 615"
           d="M32.75,0A32.75,32.75,0,1,0,65.5,32.75,32.75,32.75,0,0,0,32.75,0Zm0,57.313A24.563,24.563,0,1,1,57.313,32.75,24.591,24.591,0,0,1,32.75,57.313Z"
           transform="translate(20 723)"
         />
